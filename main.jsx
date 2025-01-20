2672519fc3c2fcf49b2e9954b7b261ab6d14a190
Haarini Govindaraj <146501904+Haarinigovindaraj15082004@users.noreply.github.com>
2025-01-20 23:29:54 +0530

    Removed unnecessary lines of code

--- main.jsx
+++ main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//  import './index.css'
 import './Qrcode.css'
 import { Qrcode } from './Qrcode'
-//  import { UserCard } from './UserCard'
-// import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <UserCard/> */}
     <Qrcode/>
   </StrictMode>,
 )
